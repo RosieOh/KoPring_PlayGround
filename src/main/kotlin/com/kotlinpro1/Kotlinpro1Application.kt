b01c2b5524716a97fede0266ef1c3c4f58b65fec
[RosieOh] <[20172207@gm.hannam.ac.kr]>
2024-04-21 02:37:00 +0900

    ✨BUILD : Kotlin+SpringBoot = Kopring Init 세팅

---
+++ src/main/kotlin/com/kotlinpro1/Kotlinpro1Application.kt
@@ -0,0 +1,11 @@
+package com.kotlinpro1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlinpro1Application
+
+fun main(args: Array<String>) {
+    runApplication<Kotlinpro1Application>(*args)
+}
