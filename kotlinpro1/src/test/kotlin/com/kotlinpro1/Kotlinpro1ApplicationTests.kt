3624b7d5a6900f4206cb13df3c0a0acf34d0e93a
RosieOh <20172207@gm.hannam.ac.kr>
2024-04-21 03:08:10 +0900

    ✨CHORE : 스테이징 중복으로 인한 수정

---
+++ kotlinpro1/src/test/kotlin/com/kotlinpro1/Kotlinpro1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinpro1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlinpro1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
